21731fadec191d0cb01ca0ef9edfc81e4b61ad71
MC <Malckira@gmail.com>
2022-04-14 11:12:33 +0200

    url

---
+++ frontend/src/components/Url/url.jsx
@@ -0,0 +1,3 @@
+export const LOGIN_URL = "http://localhost:3001/api/v1/user/login"
+export const EDIT_PROFILE_URL = "http://localhost:3001/api/v1/user/profile"
+export const signupApi = 'http://localhost:3001/api/v1/user/signup'
