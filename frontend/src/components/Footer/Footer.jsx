c1444cae72a01a72da3c753792da3517f6e4b7e3
MC <Malckira@gmail.com>
2022-04-14 11:10:51 +0200

    fix footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
 export default function Footer(){
     return(
-        <p>
+        <div className="footer">
+
+        <p className="footer-text">
             Copyright 2020 Argent Bank
         </p>
+        </div>
     )
 }
